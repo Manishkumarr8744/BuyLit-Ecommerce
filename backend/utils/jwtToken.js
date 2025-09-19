f73bce6cd8c539b7378e6b059330776554388de0
Manishkumarr8744 <mk7219630@gmail.com>
2025-09-19 13:50:32 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,10 +4,10 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     httpOnly: true,
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      24 * 60 * 60 * 1000
     ),
-    secure: process.env.NODE_ENV === "PRODUCTION",
-    sameSite: process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
+    secure: true,
+    sameSite:'none',
   };
 
   res.status(statusCode).cookie("token", token, options).json({
