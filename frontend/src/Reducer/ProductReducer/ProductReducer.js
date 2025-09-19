60543223fa6055b73626348381b63971af130ce4
Manishkumarr8744 <mk7219630@gmail.com>
2025-09-19 18:58:52 +0530

    Update ProductReducer.js

--- frontend/src/Reducer/ProductReducer/ProductReducer.js
+++ frontend/src/Reducer/ProductReducer/ProductReducer.js
@@ -20,10 +20,11 @@ const ProductReducer = createSlice({
     adminProductRequest(state) {
       state.loading = true;
       
+      
     },
     allProductSuccess(state, action) {
       state.loading = false;
-      state.products = action.payload;
+      state.products = action.payload.products;
       state.productsCount = action.payload.productsCount;
       state.resultPerPage = action.payload.resultPerPage;
       state.filteredProductsCount = action.payload.filteredProductsCount;
